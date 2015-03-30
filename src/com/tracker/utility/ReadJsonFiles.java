68ef78e3d4d4ce005ad1c8fbad40817a511c112d
Tahmina Khan <tahmina.khan@gmail.com>
2015-03-30 15:58:11 -0400

    Refactored

--- src/com/tracker/utility/ReadJsonFiles.java
+++ src/com/tracker/utility/ReadJsonFiles.java
@@ -1,7 +1,15 @@
 package com.tracker.utility;
 
 import com.google.gson.stream.JsonReader;
+import com.tracker.model.Issue;
+import com.tracker.model.Mainmenu;
+import com.tracker.model.Make;
+import com.tracker.model.Model;
 import com.tracker.model.Notes;
+import com.tracker.model.Script;
+import com.tracker.model.Scripts;
+import com.tracker.model.Step;
+import com.tracker.model.Submenu;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -62,8 +70,370 @@ public class ReadJsonFiles {
         }
         reader.endObject();
         noteobject = new Notes(id, notetitle, notes);
-        //System.out.println(noteobject.toString());
+       
         return noteobject;
+    }
+
+    
+     /**
+     *
+     *
+     * @param jsonFileName
+     * @return Scripts that read from the file
+     */
+    public static Scripts readScriptsJson(String jsonFileName) throws IOException {
+        JsonReader reader = new JsonReader(new FileReader(jsonFileName));
+        try {
+            return readScriptObject(reader);
+        } finally {
+            reader.close();
+        }
+
+    }
+
+    /**
+     *
+     * @param reader
+     * @return
+     * @throws IOException
+     *
+     * reading the actual Script object from the json
+     *
+     */
+    private  static Scripts readScriptObject(JsonReader reader) throws IOException {
+
+        Scripts readScripts = new Scripts();
+        String name = "";
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+            name = reader.nextName();
+            if (name.equals("scripts")) {
+
+                // reading menus
+                readScripts.setMenus(readMenuList(reader));
+
+            } else {
+                reader.skipValue();
+            }
+        }
+
+        reader.endObject();
+
+        return readScripts;
+    }
+
+    private static List<Mainmenu> readMenuList(JsonReader reader) throws IOException {
+
+        List<Mainmenu> menus = new ArrayList<Mainmenu>();
+        Mainmenu menu = null;
+        int id = -1;
+        String menuName = "";
+        List<Submenu> submenus = null;
+
+        reader.beginArray();
+        while (reader.hasNext()) {
+            menus.add(readMainMenus(reader));
+
+        }
+
+        reader.endArray();
+        return menus;
+    }
+
+    // read each menu,and add to manu list
+    private static Mainmenu readMainMenus(JsonReader reader) throws IOException {
+
+        Mainmenu menu = null;
+        int id = -1;
+        String menuname = "";
+        List<Submenu> submenus = null;
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+
+            String name = reader.nextName();
+            if (name.equals("id")) {
+                id = reader.nextInt();
+            } else if (name.equals("menuname")) {
+                menuname = reader.nextString();
+            } else if (name.equals("submenus")) {
+
+                submenus = readSubMenus(reader);
+            } else {
+                reader.skipValue();
+            }
+
+        }
+
+        reader.endObject();
+
+        menu = new Mainmenu(id, menuname, submenus);
+
+        return menu;
+
+    }
+
+    private static List<Submenu> readSubMenus(JsonReader reader) throws IOException {
+
+        List<Submenu> submenu = new ArrayList<Submenu>();
+
+        reader.beginArray();
+        while (reader.hasNext()) {
+
+            submenu.add(readSubMenu(reader));
+        }
+
+        reader.endArray();
+
+        return submenu;
+    }
+
+    private static Submenu readSubMenu(JsonReader reader) throws IOException {
+
+        Submenu submenu = null;
+        String submenuname = "";
+        String submenutitle = "";
+        List<Script> submenuscripts = null;
+        String name = "";
+        reader.beginObject();
+        while (reader.hasNext()) {
+
+            name = reader.nextName();
+
+            if (name.equals("submenuname")) {
+                submenuname = reader.nextString();
+            } else if (name.equals("submenutitle")) {
+                submenutitle = reader.nextString();
+            } else if (name.equals("submenusripts")) {
+                submenuscripts = readSubMenuScripts(reader);
+            } else {
+                reader.skipValue();
+            }
+
+        }
+        reader.endObject();
+
+        submenu = new Submenu(submenuname, submenutitle, submenuscripts);
+
+        return submenu;
+
+    }
+
+    private static List<Script> readSubMenuScripts(JsonReader reader) throws IOException {
+
+        List<Script> submenus = new ArrayList<Script>();
+
+        reader.beginArray();
+        while (reader.hasNext()) {
+            submenus.add(readSubmenuScript(reader));
+        }
+
+        reader.endArray();
+
+        return submenus;
+
+    }
+
+    private static Script readSubmenuScript(JsonReader reader) throws IOException {
+
+        String title = "";
+        String scripttext = "";
+        Script script = null;
+        String name = "";
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+            name = reader.nextName();
+            if (name.equals("scripttitle")) {
+                title = reader.nextString();
+            } else if (name.equals("script")) {
+                scripttext = reader.nextString();
+            } else {
+                reader.skipValue();
+            }
+
+        }
+
+        reader.endObject();
+
+        return new Script(title, scripttext);
+
+    }
+    
+    
+
+    public static List<Make> readDevicesJson(String jsonFileName) throws IOException {
+        JsonReader reader = new JsonReader(new FileReader(jsonFileName));
+        try {
+            return readMakesList(reader);
+        } finally {
+            reader.close();
+        }
+
+    }
+
+    private static List<Make> readMakesList(JsonReader reader) throws IOException {
+
+        List<Make> makes = new ArrayList<Make>();
+
+        reader.beginArray();
+        while (reader.hasNext()) {
+            makes.add(readMake(reader));
+
+        }
+
+        reader.endArray();
+        return makes;
+    }
+
+    private static Make readMake(JsonReader reader) throws IOException {
+
+        String name = "";
+
+        int id = -1;
+        String makeName = "";
+        List<Model> models = null;
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+
+            name = reader.nextName();
+
+            if (name.equals("id")) {
+                id = reader.nextInt();
+            } else if (name.equals("make")) {
+                makeName = reader.nextString();
+            } else if (name.equals("modeloros")) {
+                models = readModels(reader);
+            } else {
+                reader.skipValue();
+            }
+
+        }
+        reader.endObject();
+
+        return new Make(id, makeName, models);
+
+    }
+
+    private static List<Model> readModels(JsonReader reader) throws IOException {
+
+        List<Model> models = new ArrayList<Model>();
+
+        reader.beginArray();
+        while (reader.hasNext()) {
+            models.add(readModel(reader));
+        }
+        reader.endArray();
+
+        return models;
+
+    }
+
+    private static Model readModel(JsonReader reader) throws IOException {
+
+        String name = "";
+
+        String modelName = "";
+        List<Issue> issues = null;
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+
+            name = reader.nextName();
+
+            if (name.equals("modelorosname")) {
+                modelName = reader.nextString();
+            } else if (name.equals("issues")) {
+                issues = readIssues(reader);
+            } else {
+                reader.skipValue();
+            }
+
+        }
+        reader.endObject();
+
+        return new Model(modelName, issues);
+    }
+
+    private static List<Issue> readIssues(JsonReader reader) throws IOException {
+        List<Issue> issues = new ArrayList<Issue>();
+
+        reader.beginArray();
+        while (reader.hasNext()) {
+            issues.add(readIssue(reader));
+        }
+
+        reader.endArray();
+
+        return issues;
+    }
+
+    private static Issue readIssue(JsonReader reader) throws IOException {
+
+        Issue issue = null;
+
+        String name = "";
+
+        String issueName = "";
+        List<Step> steps = new ArrayList<Step>();
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+
+            name = reader.nextName();
+
+            if (name.equals("issue")) {
+                issueName = reader.nextString();
+            } else if (name.equals("steps")) {
+                steps = readSteps(reader);
+            } else {
+                reader.skipValue();
+            }
+
+        }
+        reader.endObject();
+
+        return new Issue(issueName, steps);
+    }
+
+    private static List<Step> readSteps(JsonReader reader) throws IOException {
+        List<Step> steps = new ArrayList<Step>();
+
+        reader.beginArray();
+        while (reader.hasNext()) {
+            steps.add(readStep(reader));
+        }
+        reader.endArray();
+
+        return steps;
+    }
+
+    private static Step readStep(JsonReader reader) throws IOException {
+
+        String name = "";
+        String stepTitle = "";
+        String stepText = "";
+
+        reader.beginObject();
+
+        while (reader.hasNext()) {
+
+            name = reader.nextName();
+
+            if (name.equals("steptitle")) {
+                stepTitle = reader.nextString();
+            } else if (name.equals("step")) {
+                stepText = reader.nextString();
+            } else {
+                reader.skipValue();
+            }
+
+        }
+        reader.endObject();
+
+        return new Step(stepTitle, stepText);
     }
 
 }
